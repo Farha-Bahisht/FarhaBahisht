2aa2a964e6aebdbde4d9207038bf56468e44a378
Farha-Bahisht <farha.bahisht@gmail.com>
2025-07-02 00:42:27 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const texts = ['a Developer', 'an Android Engineer', 'a Problem Solver'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+  if (count === texts.length) count = 0;
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.getElementById('typing').textContent = letter;
+
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+    setTimeout(type, 2000);
+  } else {
+    setTimeout(type, 150);
+  }
+})();
